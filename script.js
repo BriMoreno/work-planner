55fb572c8647744b905a11002fe5987df748e054
Britania Moreno <69614167+BriMoreno@users.noreply.github.com>
2022-07-11 19:22:26 -0700

    added variables

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ $("#currentDay").text(today.format("MMM Do, YYYY"));
 
 var time = moment()
 $("#currentTime").text(time.format('LTS'));
+
+var hourDisplay = [
+    "6AM",
+    "7AM",
+    "8AM",
+    "9AM",
+    "10AM",
+    "11AM",
+    "12PM",
+    "1PM",
+    "2PM",
+    "3PM",
+    "4PM",
+    "5PM",
+    "6PM"
+  ];
